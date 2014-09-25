0723d3f6e7cc7534d9ad07a6270d3f17cb6ec5b3
Michael Youngblood <gmyoungblood@gmail.com>
2014-09-25 14:33:53 -0700

    Initial Commit

---
+++ CLIPS/CLIPS.h
@@ -0,0 +1,13 @@
+//
+//  CLIPS.h
+//  CLIPS
+//
+//  Created by Michael Youngblood on 9/25/14.
+//  Copyright (c) 2014 gmyoungblood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLIPS : NSObject
+
+@end
---
+++ CLIPS/CLIPS.m
@@ -0,0 +1,13 @@
+//
+//  CLIPS.m
+//  CLIPS
+//
+//  Created by Michael Youngblood on 9/25/14.
+//  Copyright (c) 2014 gmyoungblood. All rights reserved.
+//
+
+#import "CLIPS.h"
+
+@implementation CLIPS
+
+@end
